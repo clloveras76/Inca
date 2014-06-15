3b0e06716d3839ed9ff4655ebd26a69e28c4abc7
clloveras76 <celloveras@hotmail.com>
2014-06-15 18:36:07 -0300

    Commit_18_formu
    Commit_18_formu

--- Material/contact.php
+++ Material/contact.php
@@ -1,39 +1,33 @@
-
-<style rel="stylesheet" type="text/css">
-body {
-color:#fff;
-}
-</style>
-
-
 <?php
-    $to = "recepcion@textilibero.com";  
+    $to = "carloslloveras@gmail.com";  
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $headers = "From: $from";
-    $subject = "Nuevo mensaje desde TextilIbero.com";
+    $subject = "Nuevo mensaje desde Inca.com";
 
     $fields = array();
     $fields{"name"} = "name";
     $fields{"email"} = "email";
     $fields{"message"} = "message";
 
-    $body = 'Contenido del mensaje:\n\n'; 
+    $body = "Contenido del mensaje:\n\n"; 
 	
-	foreach($fields as $a => $b){   $body .= sprintf('%20s: %s\n',$b,$_REQUEST[$a]); }
+	foreach($fields as $a => $b){$body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]);}
 
     $send = mail($to, $subject, $body, $headers);
 	
-	header('Location: pag2.asp');
+/*	header('Location: pag2.asp');*/
 	
 ?>
 
-    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
 		
 	<script type="text/javascript">
-
+/*
 	$(window).load(function(){
 
-	$(location).attr('href',"confirmacionmensaje.html"); 
+	$(location).attr('href',"index.html"); 
 	}) 
+*/	
+	
 	</script>
